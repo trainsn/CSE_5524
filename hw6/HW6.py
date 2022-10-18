768e6636b27253357b89e3bce3edd95d3dd26034
Neng Shi <shineng@zju.edu.cn>
2022-10-18 17:30:19 -0500

    finish hw6

--- hw6/HW6.py
+++ hw6/HW6.py
@@ -36,13 +36,82 @@ def task1():
             dist = np.sqrt((np.log(eigvals)**2).sum())
             dists[i, j] = dist
 
-    pdb.set_trace()
     minPos = dists.argmin()
     minPos = minPos // (W - window_cols), minPos % (W - window_cols)
     plt.imshow(dists, cmap="gray")
     plt.show()
+    print(minPos)
     imageio.imwrite("best_match.jpg", target[minPos[0]:minPos[0] + window_rows, minPos[1]:minPos[1] + window_cols])
 
+def circularNeighbors(img, x, y, radius):
+    H, W, _ = img.shape
+    X = []
+    for i in range(H):
+        for j in range(W):
+            if (i - y) ** 2 + (j - x) ** 2 < radius ** 2:
+                X.append(np.array([j, i, img[i, j, 0], img[i, j, 1], img[i, j, 2]]))
 
+    return np.array(X).astype(np.float32)
+
+def colorHistogram(X, bins, x, y, h):
+    hist = np.zeros((bins, bins, bins), dtype=np.float32)
+    step = (255. + 1e-2) / bins
+    for i in range(X.shape[0]):
+        feat = X[i]
+        r_idx, g_idx, b_idx = int(feat[2] // step), int(feat[3] // step), int(feat[4] // step)
+        px, py = feat[0], feat[1]
+        r = ((py - y) ** 2 + (px - x) ** 2) / (h ** 2)
+        hist[r_idx, g_idx, b_idx] += 1 - r
+
+    return hist / hist.sum()
+
+def meanshiftWeights(X, q_model, p_test, bins):
+    div = np.zeros((bins, bins, bins), dtype=np.float32)
+    eps = 1e-4
+    valid = p_test >= eps
+    div[valid] = np.sqrt(q_model[valid] / p_test[valid])
+
+    w = []
+    step = (255. + 1e-2) / bins
+    for i in range(X.shape[0]):
+        feat = X[i]
+        r_idx, g_idx, b_idx = int(feat[2] // step), int(feat[3] // step), int(feat[4] // step)
+        w.append(div[r_idx, g_idx, b_idx])
+
+    return np.array(w)
+
+def task5():
+    img1 = imageio.imread("img1.jpg")
+    img2 = imageio.imread("img2.jpg")
+    cx, cy = 150., 175.
+    radius = 25.
+    bins = 16
+    num_iter = 25
+
+    plt.imshow(img1)
+    circle1 = plt.Circle((cx, cy), radius=25, color='b', fill=False)
+    plt.gcf().gca().add_artist(circle1)
+    plt.axis('off')
+    plt.show()
+
+    cNeigh = circularNeighbors(img1, cx, cy, radius)
+    q_model = colorHistogram(cNeigh, bins, cx, cy, radius)
+
+    old_loc = np.array([cx, cy])
+    for i in range(num_iter):
+        cNeigh2 = circularNeighbors(img2, old_loc[0], old_loc[1], radius)
+        p_test = colorHistogram(cNeigh2, bins, old_loc[0], old_loc[1], radius)
+        w = meanshiftWeights(cNeigh2, q_model, p_test, bins)
+        new_loc = (w[:, np.newaxis] * cNeigh2[:, :2]).sum(axis=0) / w.sum()
+        print(new_loc, np.sqrt(((new_loc - old_loc) ** 2).sum()))
+        old_loc = new_loc
+
+    plt.clf()
+    plt.imshow(img2)
+    circle2 = plt.Circle((new_loc[0], new_loc[1]), radius=25, color='b', fill=False)
+    plt.gcf().gca().add_artist(circle2)
+    plt.axis('off')
+    plt.show()
 
 task1()
+task5()
